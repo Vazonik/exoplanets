3db1ec9947f22b51ed6a2d29c52e22227293e4f9
Vazonik <dawid.dragula@gmail.com>
2021-02-12 02:48:51 +0100

    Updates

---
+++ precache-manifest.66390ec0b05a843feb2477205fc73508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4737508b724b4b1ae819dcb40c8bb1e8",
+    "url": "/exoplanets/index.html"
+  },
+  {
+    "revision": "fe4aed4481bdae5f96ef",
+    "url": "/exoplanets/static/css/main.956bfb2f.chunk.css"
+  },
+  {
+    "revision": "5b79d448497cd4d36fc9",
+    "url": "/exoplanets/static/js/2.c85e53a6.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/exoplanets/static/js/2.c85e53a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4aed4481bdae5f96ef",
+    "url": "/exoplanets/static/js/main.ecce8c0a.chunk.js"
+  },
+  {
+    "revision": "9e69130546f2dba53d9a",
+    "url": "/exoplanets/static/js/runtime-main.526da394.js"
+  }
+]);
